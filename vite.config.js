454ae4c23bc53f2cf590fbe6446628c9021a5dad
AlejandrVilla <avillah@unsa.edu.pe>
2025-01-08 11:20:24 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://AlejandrVilla.github.io/',
+  base: 'https://AlejandrVilla.github.io/space-tourism-website/',
 })
